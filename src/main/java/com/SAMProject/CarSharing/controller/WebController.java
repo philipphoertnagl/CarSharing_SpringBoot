95c02a65a662ce94f942544b68220bcfcc065267
Philipp Hoertnagl <ic22b116@technikum-wien.at>
2024-04-24 14:00:35 +0200

    success.html added

--- src/main/java/com/SAMProject/CarSharing/controller/WebController.java
+++ src/main/java/com/SAMProject/CarSharing/controller/WebController.java
@@ -3,9 +3,12 @@ package com.SAMProject.CarSharing.controller;
 import com.SAMProject.CarSharing.persistence.entity.CustomerDetails;
 import com.SAMProject.CarSharing.persistence.entity.User;
 import com.SAMProject.CarSharing.persistence.repository.UserRepository;
+import com.SAMProject.CarSharing.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -13,11 +16,11 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @Controller
 public class WebController {
 
-    private final UserRepository userRepository;
+    private final UserService userService;
 
     @Autowired
-    public WebController(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    public WebController(UserService userService) {
+        this.userService = userService;
     }
 
     @GetMapping("/register")
@@ -26,23 +29,21 @@ public class WebController {
     }
 
     @PostMapping("/register")
-    public String registerUser(@RequestParam String username, @RequestParam String password,
-                               @RequestParam User.Role role, @RequestParam(required = false) String firstName,
-                               @RequestParam(required = false) String surname, @RequestParam(required = false) Integer age,
-                               @RequestParam(required = false) String drivingLicense, @RequestParam(required = false) Integer ccNumber,
-                               RedirectAttributes redirectAttributes) {
-        User user = new User(username, password);
-        user.setRole(role);
-        if (role == User.Role.CUSTOMER) {
-            CustomerDetails customerDetails = new CustomerDetails(firstName, surname, age, drivingLicense, ccNumber);
-            user.setCustomerDetails(customerDetails);
-        }
-        User savedUser = userRepository.saveOrUpdate(user);
-        if (savedUser == null) {
-            redirectAttributes.addFlashAttribute("message", "Failed to register user.");
+    public String registerUserForm(@ModelAttribute User user, RedirectAttributes redirectAttributes) {
+        ResponseEntity<?> response = userService.registerUser(user);
+        if (!response.getStatusCode().is2xxSuccessful()) {
+            redirectAttributes.addFlashAttribute("error", response.getBody());
             return "redirect:/register";
         }
-        return "redirect:/login";
+        redirectAttributes.addFlashAttribute("username", user.getUsername());
+        redirectAttributes.addFlashAttribute("success", "Successfully registered " + user.getUsername());
+        return "redirect:/registersuccess";
     }
+
+    @GetMapping("/registersuccess")
+    public String registrationSuccess() {
+        return "registersuccess";
+    }
+
 
 }
