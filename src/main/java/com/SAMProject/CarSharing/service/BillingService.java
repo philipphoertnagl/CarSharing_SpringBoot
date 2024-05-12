d1c48e23c8028714716c326738146ba86767108a
Philipp Hoertnagl <ic22b116@technikum-wien.at>
2024-05-12 20:41:51 +0200

    vehicleToken field added to vehicle, Controller/Services adapted

---
+++ src/main/java/com/SAMProject/CarSharing/service/BillingService.java
@@ -0,0 +1,16 @@
+package com.SAMProject.CarSharing.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BillingService {
+    private static final Logger logger = LoggerFactory.getLogger(BillingService.class); //for logging with slf4j
+
+
+
+
+
+
+}
